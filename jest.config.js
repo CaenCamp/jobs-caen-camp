a99646d444be8e29fa44ef86ecbddb557933a3f3
Alexis Janvier <contact@alexisjanvier.net>
2019-12-28 11:22:13 +0100

    test: configuration de Jest pour le monorepo
    C'est à dire lancement des tests par projet

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+// eslint-disable-next-line no-undef
+module.exports = {
+    clearMocks: true,
+    // The test environment that will be used for testing
+    testEnvironment: 'node',
+    // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+    testPathIgnorePatterns: ['/node_modules/'],
+    verbose: true,
+    projects: ['<rootDir>/apps/api/', '<rootDir>/apps/front/']
+};
