2c40389b4673b0264ee103132b5ad136ab82fb0e
Alexis Janvier <contact@alexisjanvier.net>
2019-12-31 15:35:56 +0100

    feat: premier composant svelte
    et test d'un state

--- apps/front/src/components/App.svelte
+++ apps/front/src/components/App.svelte
@@ -1,5 +1,19 @@
 <script>
-  export let name;
+    export let name;
+    import Company from './Company.svelte';
+    let companyName = 'CaenCamp';
+    const changeCompanyName = () => {
+        const newName = companyName === 'CaenCamp' ? 'Coding CaenCamp' : 'CaenCamp';
+        companyName = newName;
+    };
 </script>
 
+<style>
+    h1 {
+        color: purple;
+    }
+</style>
+
 <h1>Hello {name}!</h1>
+<Company name={companyName} />
+<button on:click={changeCompanyName}>Changer le nom de la boite</button>
---
+++ apps/front/src/components/Company.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let name;
+</script>
+
+<style>
+    h3 {
+        color: blue;
+    }
+</style>
+
+
+
+<h3>Nom de l'entreprise: {name}!</h3>
