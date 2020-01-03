16f2cf572c46524463fde47611b18df0f6bacaa1
Alexis Janvier <contact@alexisjanvier.net>
2020-01-03 08:55:03 +0100

    feat: premières requêtes vers une API
    On choppe des listes sur l'API https://rickandmortyapi.com/documentation/\#introduction

---
+++ apps/front/src/rickAndMorty/api.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+
+const api = axios.create({
+    method: 'get',
+    baseURL: 'https://rickandmortyapi.com/api/',
+    timeout: 5000,
+});
+
+const validate = qry => {
+    if (
+        (typeof qry === 'number' && Number.isInteger(qry)) ||
+        Array.isArray(qry)
+    ) {
+        return `/${qry}`;
+    }
+
+    if (typeof qry === 'object') {
+        return `/?${Object.keys(qry)
+            .map(
+                key =>
+                    `${encodeURIComponent(key)}=${encodeURIComponent(qry[key])}`
+            )
+            .join('&')}`;
+    }
+
+    throw new Error(
+        'As argument use an object, an array, an integer or leave it blank'
+    );
+};
+
+const get = async (endpoint = '', opt = {}) => {
+    const query = validate(opt);
+
+    try {
+        const { data } = await api(endpoint + query);
+        return data;
+    } catch (e) {
+        console.log(e.message);
+        return {
+            status: e.response.status,
+            error: e.response.data.error,
+        };
+    }
+};
+
+export const getEndpoints = () => get();
+export const getCharacter = (opt = {}) => get('character', opt);
+export const getLocation = (opt = {}) => get('location', opt);
+export const getEpisode = (opt = {}) => get('episode', opt);
--- apps/front/src/rickAndMorty/episodes/List.svelte
+++ apps/front/src/rickAndMorty/episodes/List.svelte
@@ -1,31 +1,35 @@
+<script>
+    import { getEpisode } from '../api.js';
+    
+    const fetchEpisodes = (async () => {
+        const response = await getEpisode();
+        return response.results;
+    })()
+</script>
+
 <section class="mw7 center">
-    <article class="pv4 bt bb b--black-10 ph3 ph0-l">
-        <div class="flex flex-column flex-row-ns">
-            <div class="w-100 w-60-ns pr3-ns order-2 order-1-ns">
-                <h1 class="f3 athelas mt0 lh-title">
-                    Tech Giant Invests Huge Money to Build a Computer Out of
-                    Science Fiction
-                </h1>
-                <p class="f5 f4-l lh-copy athelas">
-                    The tech giant says it is ready to begin planning a quantum
-                    computer, a powerful cpu machine that relies on subatomic
-                    particles instead of transistors.
-                </p>
-            </div>
-            <div class="pl3-ns order-1 order-2-ns mb4 mb0-ns w-100 w-40-ns">
-                <img
-                    src="http://mrmrs.github.io/photos/cpu.jpg"
-                    class="db"
-                    alt="Photo of a dimly lit room with a computer interface
-                    terminal."
-                />
-            </div>
-        </div>
-        <p class="f6 lh-copy gray mv0">
-            By
-            <span class="ttu">Robin Darnell</span>
-        </p>
-        <time class="f6 db gray">Nov. 21, 2016</time>
-    </article>
+    {#await fetchEpisodes}
+        <p>...waiting</p>
+    {:then episodes}
+        <header class="tc ph4">
+            <h1 class="f3 f2-m f1-l fw2 black-90 mv3">
+                Les épisodes
+            </h1>
+        </header>
+        <article class="ba b--black-10 br2 bg-white pa4 mw6 center">
+            <ul class="list f6 pl0 mt3 mb0">
+                {#each episodes as episode}
+                <li class="pv2">
+                    <a href="/" class="link blue lh-title">
+                        <span class="fw7 underline-hover">Episode {episode.episode} : {episode.name}</span>
+                        <span class="db black-60">{episode.air_date}</span>
+                    </a>
+                </li>
+                {/each}
+            </ul>
+        </article>
+    {:catch error}
+        <p>An error occurred!</p>
+    {/await}
 </section>
 
