219545d63108cc6604b8b87ac22b1d709d9c4909
Alexis Janvier <contact@alexisjanvier.net>
2020-03-22 10:41:29 +0100

    chore: bootstrap de react-admin
    Mise en place des CORS sur l'API
    Utilisation des ListGuesser sur les entreprises et les offres

--- not-in-yarn-workspace/admin/src/App.js
+++ not-in-yarn-workspace/admin/src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Admin, Resource, ListGuesser } from "react-admin";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import jobBoardDataProvider from "./jobBoardDataProvider";
+
+const dataProvider = jobBoardDataProvider("http://localhost:8001/api");
+const App = () => (
+    <Admin dataProvider={dataProvider}>
+        <Resource name="organizations" list={ListGuesser} />
+        <Resource name="job-postings" list={ListGuesser} />
+    </Admin>
+);
 
 export default App;
