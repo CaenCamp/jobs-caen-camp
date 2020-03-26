bc618d7d2d01c41b5628a76b386b47e5d932d545
Nexoid <marvin.anfray@gmail.com>
2020-03-26 12:39:41 +0100

    fix(main): fixed import filename

--- apps/front/src/main.js
+++ apps/front/src/main.js
@@ -1,8 +1,7 @@
-import App from './App.svelte';
+import App from './app.svelte';
 
 const app = new App({
     target: document.body,
     hydrate: false,
 });
-console.log('ici');
 export default app;
