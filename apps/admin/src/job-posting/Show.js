2944b60f23123a5ee3b1696a16533b6279de7be1
Emmanuel Bosquet <bjokac@gmail.com>
2020-06-03 14:03:53 +0200

    feat(admin): page fixes

--- apps/admin/src/job-posting/Show.js
+++ apps/admin/src/job-posting/Show.js
@@ -5,30 +5,23 @@ export const JobPostingShow = (props) => {
     return (
         <Show title="Vue de l'offre d'emploi" {...props}>
             <SimpleShowLayout>
-                <TextField source="title:%l%" label="Filtre par titre" />
+                <TextField source="title" label="Filtre par titre" />
                 <TextField source="employmentType" label="Type de contrat" />
                 <TextField
-                    source="hiringOrganizationName:%l%"
+                    source="hiringOrganizationName"
                     label="Nom d'entreprise"
                 />
                 <TextField
-                    source="hiringOrganizationAddressLocality:l%"
+                    source="hiringOrganizationAddressLocality"
                     label="Ville de l'entreprise"
                 />
-                <TextField
-                    source="hiringOrganizationPostalCode:l%"
+                <DateField
+                    source="hiringOrganizationPostalCode"
                     label="Code postal de l'entreprise"
                 />
 
-                <DateField source="datePosted:lte" label="Postée avant le" />
-                <TextField source="datePosted:gte" label="Postée après le" />
-                <TextField source="jobStartDate:lte" label="Commence avant" />
-                <TextField source="jobStartDate:gte" label="Commence après" />
-                <TextField
-                    source="validThrough_before"
-                    label="Valide jusqu'au"
-                />
-                <TextField source="validThrough:gte" label="Valide après le" />
+                <DateField source="jobStartDate" label="Commence avant" />
+                <DateField source="validThrough" label="Valide jusqu'au" />
             </SimpleShowLayout>
         </Show>
     );
